4225155c41b8a18087cdc8ad61a3d5ffe15b5556
Chaverondier Thomas <tchaverond@bs406-s16-11.insa-lyon.fr>
2014-12-12 16:22:12 +0100

    added Heun scheme to gyllenberg model
    switch between different schemes with changing value of 'scheme' double

--- gyllenberg.cpp
+++ gyllenberg.cpp
@@ -17,12 +17,22 @@ In this model, if mu = 0, quiescent cells and necrosed (dead) cells both fall un
 #include <math.h>
 
 
-double ro_evaluate (double, double, double, double);  
+// evaluating dpc according to Gyllenberg-Webb model
+double dpc_evaluate (double, double, double, double, double, double);
+
+
+// evaluating dqc according to Gyllenberg-Webb model
+double dqc_evaluate (double, double, double, double, double, double, double);
+
+
 // function corresponding to the cell transformation rate from proliferent to quiescent
 // params : total number of cells (variable of the function), b (growth factor) ; and alpha and teta from gompertz or verhulst model
+double ro_evaluate (double, double, double, double);  
 
-double ri_evaluate (double);  // function corresponding to the cell transformation rate from quiescent to proliferent
+
+// function corresponding to the cell transformation rate from quiescent to proliferent
 // params : total number of cells (variable of the function)
+double ri_evaluate (double);  
 
 
 
@@ -32,14 +42,20 @@ int main (int argc, char* argv[])
   FILE* output;
   output = fopen("gyllenberg.txt","w");
 
+  double scheme = 0;       // defines which scheme will be used to solve the equations (0 = Euler expl., 1 = Heun (trapeziums))
+
   double ipc = 1;          // initial number of proliferating cells
   double iqc = 1;          // initial number of quiescent cells
   double itc = ipc + iqc;  // initial total number of cells
   double pc = ipc;         // number of proliferating cells
   double qc = iqc;         // number of quiescent cells
   double tc = pc + qc;     // total number of cells
-  double dpc;              // pc derivative
-  double dqc;              // qc derivative
+  //double dpc;              // pc derivative
+  //double dqc;              // qc derivative
+
+  double temp_pc;          // temporary approximation of pc (used for trapezium method)
+  double temp_qc;          // temporary approximation of qc
+  double temp_tc;          // temporary approximation of tc
 
 
   // implicit parameters if using gompertz or verhuslt as the ro function
@@ -50,9 +66,9 @@ int main (int argc, char* argv[])
   double b;              // effective growth factor (births - deaths)
   double mu = 0;         // death rate of quiescent cells (equals 0 if quiescent cells and necrosed (dead) cells both fall under the 'quiescent' category)
 
-  double step = 0.001;
+  double step = 0.01;
   double i;
-  double length = 80;    // simulation length
+  double length = 10;    // simulation length
 
 
   // specific parameter initialization
@@ -63,17 +79,54 @@ int main (int argc, char* argv[])
 
   for (i=0;i<length;i+=step)
     {
-      dpc = pc * (b - ro_evaluate(tc,b,alpha,teta)) + qc * ri_evaluate(tc);
-      dqc = pc * ro_evaluate(tc,b,alpha,teta) - qc * (mu + ri_evaluate(tc));
-      pc += step * dpc;
-      qc += step * dqc;
-      tc = pc + qc;
+      //dpc = pc * (b - ro_evaluate(tc,b,alpha,teta)) + qc * ri_evaluate(tc);
+      //dqc = pc * ro_evaluate(tc,b,alpha,teta) - qc * (mu + ri_evaluate(tc));
+
+      if (scheme == 0)
+        {
+          // Using explicit version of Euler
+          pc += step * dpc_evaluate(pc, qc, tc, b, alpha, teta);
+          qc += step * dqc_evaluate(pc, qc, tc, b, alpha, teta, mu);
+          tc = pc + qc;
+        }
+
+      if (scheme == 1)
+        {
+          // Using method of trapeziums (Heun's scheme)
+          temp_pc = pc + step * dpc_evaluate(pc, qc, tc, b, alpha, teta);
+          temp_qc = qc + step * dqc_evaluate(pc, qc, tc, b, alpha, teta, mu);
+          temp_tc = temp_pc + temp_qc;
+          pc += (step/2) * ( dpc_evaluate(pc, qc, tc, b, alpha, teta) + dpc_evaluate(temp_pc, temp_qc, temp_tc, b, alpha, teta) );
+          qc += (step/2) * ( dqc_evaluate(pc, qc, tc, b, alpha, teta, mu) + dqc_evaluate(temp_pc, temp_qc, temp_tc, b, alpha, teta, mu) );
+          tc = pc + qc;
+        }   
+
       fprintf(output,"%lg  %lg  %lg  %lg\n",i,pc,qc,tc);
     }
 
   fclose(output);
 
+  return 0;
+
 }
+
+
+double dpc_evaluate (double pc, double qc, double tc, double b, double alpha, double teta)
+{
+
+  return (pc * (b - ro_evaluate(tc,b,alpha,teta)) + qc * ri_evaluate(tc));
+
+}
+
+
+double dqc_evaluate (double pc, double qc, double tc, double b, double alpha, double teta, double mu)
+{
+
+  return (pc * ro_evaluate(tc,b,alpha,teta) - qc * (mu + ri_evaluate(tc)));
+
+}
+
+
 
 
 double ro_evaluate (double value, double b, double alpha, double teta)
